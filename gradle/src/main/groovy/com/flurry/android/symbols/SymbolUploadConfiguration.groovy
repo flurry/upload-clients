f39ad6063f0cb9c63f0fdca06aa6231ed9ca30ba
Clay Reimann <clayreimann@gmail.com>
2016-11-11 17:42:56 -0600

    Add gradle plugin to send ProGuard files to Flurry (#5)

---
+++ gradle/src/main/groovy/com/flurry/android/symbols/SymbolUploadConfiguration.groovy
@@ -0,0 +1,47 @@
+/*
+ * Copyright Yahoo Inc. 2016, see https://github.com/flurry/upload-clients/blob/master/LICENSE.txt for full details
+ */
+package com.flurry.android.symbols
+
+/**
+ * Holds the user configurable values
+ */
+class SymbolUploadConfiguration {
+    String apiKey = null
+    String token = null
+    boolean useEnvVar = true
+    String configPath = null
+    int uploadTimeout = 300
+
+    public void apiKey(String apiKey) {
+        this.apiKey = apiKey
+    }
+
+    public void token(String token) {
+        this.token = token
+    }
+
+    public void useEnvironmentVariable(boolean useEnv) {
+        this.useEnvVar = useEnv
+    }
+
+    public void configPath(String configPath) {
+        this.configPath = configPath
+    }
+
+    public void uploadTimeout(int uploadTimeout) {
+        this.uploadTimeout = uploadTimeout
+    }
+
+
+    @Override
+    public String toString() {
+        return "SymbolUploadConfiguration{" +
+                "apiKey='" + apiKey + '\'' +
+                ", token='" + token + '\'' +
+                ", useEnvVar=" + useEnvVar +
+                ", configPath='" + configPath + '\'' +
+                ", uploadTimeout=" + uploadTimeout +
+                '}';
+    }
+}
---
+++ gradle/src/main/groovy/com/flurry/android/symbols/SymbolUploadPlugin.groovy
@@ -0,0 +1,85 @@
+/*
+ * Copyright Yahoo Inc. 2016, see https://github.com/flurry/upload-clients/blob/master/LICENSE.txt for full details
+ */
+package com.flurry.android.symbols
+
+import com.android.build.gradle.api.BaseVariant
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import com.flurry.proguard.UploadProGuardMapping
+
+/**
+ * A Gradle plugin that finds the generated ProGuard file and sends it to Flurry's crash service
+ */
+class SymbolUploadPlugin implements Plugin<Project> {
+    public static final String CONFIGURATION_KEY = "flurryCrash"
+    public static final String FLURRY_UUID_KEY = "com.flurry.crash.map_id"
+
+    public static final String API_KEY = "api-key"
+    public static final String TOKEN = "token"
+    public static final String TIMEOUT = "timeout"
+
+    @Override
+    void apply(Project project) {
+        UploadProGuardMapping.logger = project.logger
+        getOrCreateConfig(project)
+
+        project.afterEvaluate {
+            SymbolUploadConfiguration config = getOrCreateConfig(project)
+            Map<String, String> configValues = evaluateConfig(config)
+            String apiKey = configValues[API_KEY]
+            String token = configValues[TOKEN]
+            int timeout = configValues[TIMEOUT].toInteger()
+
+            if (!apiKey) {
+                throw new IllegalStateException("You must provide the project's API key")
+            } else if (!token) {
+                throw new IllegalStateException("You must provide a valid token")
+            }
+
+            project.android.applicationVariants.all { BaseVariant variant ->
+                if (variant.mappingFile) {
+                    String uuid = UUID.randomUUID().toString()
+                    project.logger.lifecycle("Variant=${variant.baseName} UUID=${uuid}")
+
+                    variant.resValue "string", FLURRY_UUID_KEY, uuid
+                    variant.assemble.doFirst {
+                        UploadProGuardMapping.uploadFile(apiKey, uuid, variant.mappingFile.absolutePath, token, timeout)
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Creates a configuration container in the project for Gradle to populate
+     *
+     * @param target the project to find/create the configuration in
+     * @return the project's configuration container
+     */
+    private SymbolUploadConfiguration getOrCreateConfig(Project target) {
+        SymbolUploadConfiguration config = target.extensions.findByType(SymbolUploadConfiguration.class)
+        if (config == null) {
+            config = target.extensions.create(CONFIGURATION_KEY, SymbolUploadConfiguration.class)
+        }
+        return config
+    }
+
+    private Map<String, String> evaluateConfig(SymbolUploadConfiguration config) {
+        Map<String, String> configValues = new HashMap<>();
+
+        if (config.apiKey) {
+            configValues[API_KEY] = config.apiKey
+        }
+        if (config.token) {
+            configValues[TOKEN] = config.useEnvVar ? System.getenv(config.token) : config.token
+        }
+        configValues.put(TIMEOUT, config.uploadTimeout)
+
+        if (config.configPath != null) {
+            configValues.putAll(UploadProGuardMapping.parseConfigFile(config.configPath))
+        }
+
+        return configValues
+    }
+}
